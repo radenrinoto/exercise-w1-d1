3dcc7f6a01f833b815733bcd674bed1717785487
rinotoharto <rinotoharto@gmail.com>
2023-10-23 13:19:36 +0700

    init repo

---
+++ exercise.js
@@ -0,0 +1,35 @@
+function checkInRange(num, start, end) {
+// Enter your code below;
+}
+
+checkInRange(15,11,30) //"15 is between the range 11 and 30"
+checkInRange(20,34,51) //"20 is outside the range 34 and 51"
+
+
+function isEvenorOdd(num) {
+  // Enter your code below;
+}
+
+isEvenorOdd(10) //"10 is a Even number"
+isEvenorOdd(19) //"19 is a Odd number"
+
+function findLargest(num1, num2) {
+// Enter your code below;
+
+}
+
+findLargest(21,45) //"45 the largest number"
+findLargest(34,18) //"34 is the largest number"
+findLargest(41,41) //"41 is equal to 41"
+
+function generateScore(score) {
+// Enter your code below;
+}
+
+generateScore(75);
+// Score of less than 0 or more than 100 — "This is not possible, an error has occurred."
+// Score of 0 to 19 — "That was a terrible score — total fail!"
+// Score of 20 to 39 — "You know some things, but it\'s a pretty bad score. Needs improvement."
+// Score of 40 to 69 — "You did a passable job, not bad!"
+// Score of 70 to 89 — "That''s a great score, you really know your stuff."
+// Score of 90 to 100 — "What an amazing score! Did you cheat? Are you for real?"
