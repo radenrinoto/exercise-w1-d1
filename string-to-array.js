e3e7c684632344662e2271ff925ba563f33bb8f9
rinotoharto <rinotoharto@gmail.com>
2023-10-23 14:53:41 +0700

    create exercise 2

---
+++ string-to-array.js
@@ -0,0 +1,18 @@
+function stringToArray(string) {
+  // write your code here
+}
+
+console.log(stringToArray('aqrst,ukaei,ffooo'))
+// [
+//   [ 'a', 'q', 'r', 's', 't' ],
+//   [ 'u', 'k', 'a', 'e', 'i' ],
+//   [ 'f', 'f', 'o', 'o', 'o' ]
+// ]
+
+console.log(stringToArray('qwer,tyui,asdf,ghjk'))
+// [
+//   [ 'q', 'w', 'e', 'r' ],
+//   [ 't', 'y', 'u', 'i' ],
+//   [ 'a', 's', 'd', 'f' ],
+//   [ 'g', 'h', 'j', 'k' ]
+// ]
