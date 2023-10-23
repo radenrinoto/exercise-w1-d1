fd99cc6ff554f57b87273d775bdaacfd7f668052
Daniel Riyanto <danielriyanto190700@gmail.com>
2023-10-23 13:56:36 +0700

    Complete the exercise

--- exercise.js
+++ exercise.js
@@ -1,5 +1,9 @@
+// Nama lengkap: Daniel Riyanto
+
 function checkInRange(num, start, end) {
-// Enter your code below;
+  // Enter your code below;
+  const checkWord = (num >= start && num <= end) ? "between" : "outside";
+  console.log(`${num} is ${checkWord} the range ${start} and ${end}`);
 }
 
 checkInRange(15,11,30) //"15 is between the range 11 and 30"
@@ -8,14 +12,19 @@ checkInRange(20,34,51) //"20 is outside the range 34 and 51"
 
 function isEvenorOdd(num) {
   // Enter your code below;
+  console.log(`${num} is a ${num % 2 == 1 ? "Odd" : "Even"} number`);
 }
 
 isEvenorOdd(10) //"10 is a Even number"
 isEvenorOdd(19) //"19 is a Odd number"
 
 function findLargest(num1, num2) {
-// Enter your code below;
-
+  // Enter your code below;
+  if (num1 !== num2) {
+    console.log(`${Math.max(num1, num2)} is the largest number`);
+  } else {
+    console.log(`${num1} is equal to ${num2}`);
+  }
 }
 
 findLargest(21,45) //"45 the largest number"
@@ -23,7 +32,20 @@ findLargest(34,18) //"34 is the largest number"
 findLargest(41,41) //"41 is equal to 41"
 
 function generateScore(score) {
-// Enter your code below;
+  // Enter your code below;
+  if (score < 0 || score > 100) {
+    console.log("This is not possible, an error has occurred.");
+  } else if (score < 20) {
+    console.log("That was a terrible score — total fail!");
+  } else if (score < 40) {
+    console.log("You know some things, but it\'s a pretty bad score. Needs improvement.");
+  } else if (score < 70) {
+    console.log("You did a passable job, not bad!");
+  } else if (score < 90) {
+    console.log("That's a great score, you really know your stuff.");
+  } else { // score of 90 to 100
+    console.log("What an amazing score! Did you cheat? Are you for real?");
+  }
 }
 
 generateScore(75);
