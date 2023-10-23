4f01d2d132ec6feea95884102d382a0b60236d8e
VELDR <melvernardell@gmail.com>
2023-10-23 16:01:28 +0700

    fix checkVowel & added test cases

--- check-vowel.js
+++ check-vowel.js
@@ -1,20 +1,17 @@
 function checkVowel(string) {
   const VOWELS = 'aeiouAEIOU';
-  const result = [];
 
   for (const char of string) {
-    if (VOWELS.includes(char)) {
-      result.push(true);
-    } else {
-      result.push(false);
+    if (!VOWELS.includes(char)) {
+      return false;
     }
   }
 
-  return result;
+  return true;
 }
 
 console.log(checkVowel('alksk d'));
 console.log(checkVowel('rabsnckksola'));
 console.log(checkVowel('aiueoaiueoaiuoe'));
-console.log(checkVowel('aaaaaaaaaaadaaaa'));
-console.log(checkVowel('aASDSAuO'));
+console.log(checkVowel('aaaaaadaaaaaa'));
+console.log(checkVowel('aAeAuO'));
